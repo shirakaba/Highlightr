3181334ee38243aeaa3c9d4c207968355ae7cd6c
Jamie Birch work laptop <jbizzle5@gmail.com>
2018-11-25 21:57:16 +0000

    Trying to get either of these import syntaxes working

--- demo/app/main-view-model.ts
+++ demo/app/main-view-model.ts
@@ -1,39 +1,57 @@
 import { Observable } from "tns-core-modules/data/observable";
+import { TextField } from "tns-core-modules/ui/text-field";
+import { TextView } from "tns-core-modules/ui/text-view";
+// import { Highlightr } from "nativescript-syntax-highlighter/typings/objc!Highlightr.d.ts";
+import { SyntaxHighlighter } from "nativescript-syntax-highlighter/index.ios.d.ts";
 
 export class HelloWorldModel extends Observable {
-
-    private _counter: number;
-    private _message: string;
+    // private highlightr: Highlightr = Highlightr.alloc().init();
+    private syntaxHighlighter: SyntaxHighlighter = new SyntaxHighlighter();
+    private tv?: TextView;
 
     constructor() {
         super();
-
-        // Initialize default values.
-        this._counter = 42;
-        this.updateMessage();
     }
 
-    get message(): string {
-        return this._message;
-    }
-    
-    set message(value: string) {
-        if (this._message !== value) {
-            this._message = value;
-            this.notifyPropertyChange("message", value)
+    onComponentLoaded(args){
+        const view: TextView|TextField = <TextView|TextField>args.object;
+        console.log("onComponentLoaded");
+
+        switch(view.id){
+            case "tv":
+                this.tv = view as TextView;
+                // HelloWorldModel.applyAttributedText(this.tv!, "whatever");
+                this.applySyntaxHighlightedText(
+                    this.tv!,
+                    `const num = 5;\n\nfunction myFunc(param1){\n\tconsole.log("txt", param1);\n}\n\nmyFunc(num);`,
+                    "js"
+                );
+                break;
         }
     }
 
-    public onTap() {
-        this._counter--;
-        this.updateMessage();
+    applySyntaxHighlightedText(textView: TextView, text: string, lang: string, theme?: string){
+        // if(theme) this.highlightr.setThemeTo(theme);
+        // const attributedString: NSAttributedString = this.highlightr.highlightCodeAs(text, lang);
+
+        if(theme) this.syntaxHighlighter.setThemeTo(theme);
+        const attributedString: NSAttributedString = this.syntaxHighlighter.highlightCodeAs(text, lang);
+
+
+        (textView.ios as UITextView).attributedText = attributedString;
     }
 
-    private updateMessage() {
-        if (this._counter <= 0) {
-            this.message = "Hoorraaay! You unlocked the NativeScript clicker achievement!";
-        } else {
-            this.message = `${this._counter} taps left`;
-        }
+    static applyTraditionalAttributedText(textView: TextView, text: string){
+        const attributedString: NSAttributedString = NSAttributedString.alloc()
+        .initWithStringAttributes(
+            text,
+            //@ts-ignore
+            new NSDictionary(
+                [UIColor.purpleColor],
+                [NSForegroundColorAttributeName],
+            )
+        );
+
+        (textView.ios as UITextView).attributedText = attributedString;
     }
 }
--- src/typings/objc!Highlightr.d.ts
+++ src/typings/objc!Highlightr.d.ts
@@ -1,4 +1,4 @@
-export declare class Highlightr extends NSObject {
+declare class Highlightr extends NSObject {
 
 	static alloc(): Highlightr; // inherited from NSObject
 
